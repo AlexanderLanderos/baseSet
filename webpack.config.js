289f74e84e7609efb5f8b46a0068db918afc5656
Alexander Landeros <AREX@AREX-MBP.charter.com>
2020-12-23 12:42:01 -0800

    removed package-lock and added PageInterface

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
+
 const path = require('path');
+
 module.exports = {
-   entry: './src/index.tsx',
-output: {
-     path: path.join(__dirname, '/dist'),
-     filename: 'bundle.min.js'
+  mode: 'development',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.min.js'
+  },
+  module: {
+    
+    rules: [
+      { 
+        test: /\.tsx?$/, 
+        loader: 'ts-loader'
+      }
+    ]
   }
 }
